2ee718acdc95798b4fa17716c0991483231ab7a4
Jignesh Ahir <jigu015@gmail.com>
2021-08-15 16:31:29 -0700

    Restaurant Web Application

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FoodController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Auth::routes(['register'=>false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::get('/category/create', [CategoryController::class, 'create']);
+
+Route::get('/', [FoodController::class, 'listFood']);
+
+Route::resource('category', CategoryController::class)->middleware('auth');
+
+Route::resource('food', FoodController::class)->middleware('auth');
+
+Route::get('/foods/{id}', [FoodController::class, 'view'])->name('food.view');
